d222d3730185622cffd91f7a09a08024d238bc5b
TCM-dev <valentin.wojtasinski@gmail.com>
2021-02-13 19:38:27 +0100

    Material UI installed and working

---
+++ pages/index.tsx
@@ -0,0 +1,24 @@
+import { Button } from "@material-ui/core";
+import Head from "next/head";
+import React from "react";
+import styles from "../styles/Home.module.css";
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Button variant="contained">Default</Button>
+      <Button variant="contained" color="primary">
+        Primary
+      </Button>
+      <Button variant="contained" color="secondary">
+        Secondary
+      </Button>
+      <Button variant="contained" disabled>
+        Disabled
+      </Button>
+      <Button variant="contained" color="primary" href="#contained-buttons">
+        Link
+      </Button>
+    </div>
+  );
+}
